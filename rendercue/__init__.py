4773b8e12e60297a1567555a845d755113e2a5c9
Usama Bin Shahid <72216538+usamasq@users.noreply.github.com>
2025-11-28 18:50:54 +0500

    Bump version to 1.0.1

--- rendercue/__init__.py
+++ rendercue/__init__.py
@@ -9,7 +9,7 @@ from . import preferences
 bl_info = {
     "name": "RenderCue",
     "author": "Usama Bin Shahid",
-    "version": (1, 0, 0),
+    "version": (1, 0, 1),
     "blender": (4, 2, 0),
     "location": "Properties > Render > RenderCue | Video Sequencer > RenderCue | 3D Viewport > RenderCue",
     "description": "Sequence. Queue. Render. Support development: https://www.patreon.com/c/usamasq",
